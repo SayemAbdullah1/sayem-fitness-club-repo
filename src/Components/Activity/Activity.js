f051f140a293e64f1cb93a2d3231d892c98e6e65
SayemAbdullah1 <dxsayem0@gmail.com>
2022-09-29 13:05:42 +0600

    add break in activity

--- src/Components/Activity/Activity.js
+++ src/Components/Activity/Activity.js
@@ -3,18 +3,51 @@ import './Activity.css'
 
 const Activity = () => {
     return (
+
         <div className='activity-info'>
             <div className='profile'>
             <img src={require('./pic.png')} alt="" />
             <h3>Sayem Abdullah</h3>
             </div>
 
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className='info'>
+                <div>
+                    <div className='per-info'>
+                        <h2>68<span className='kg'>kg</span> </h2>
+                    </div>
+                    <span>weight</span>
+                </div>
+                <div>
+                    <div className='per-info'>
+                        <h2>5.8</h2>
+                    </div>
+                    <span>height</span>
+                </div>
+                <div>
+                    <div className='per-info'>
+                        <h2>25<span className='kg'>yrs</span> </h2>
+                    </div>
+                    <span>age</span>
+                </div>
+                
+            </div>
+
+        {/* Add a break ------------------------------*/}
+        <div className='add-break'>
+            <h3>Add a break</h3>
+            
+            <div className='break'>
+                    <button>10<span>s</span></button>
+                    <button>15<span>s</span></button>
+                    <button>20<span>s</span></button>
+                    <button>25<span>s</span></button>
+                    <button>30<span>s</span></button>
             </div>
         </div>
+
+        </div>
+
+
     );
 };
 
